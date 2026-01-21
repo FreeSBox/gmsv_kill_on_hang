019437c93f3b5c7538785987d6a5966629e3375e
tupoy-ya <tupoy-ya@users.noreply.github.com>
2026-01-22 01:56:41 +0500

    fix: Wait 100 ticks before we allow killing the server.

--- src/kill_on_hang.cpp
+++ src/kill_on_hang.cpp
@@ -14,9 +14,10 @@ namespace gm_kill_on_hang
 		while (g_running)
 		{
 			float curtime = (*g_pointers->m_globals)->curtime;
+			int tick_count = (*g_pointers->m_globals)->tickcount;
 			int player_count = g_pointers->m_sv->GetNumPlayers();
 
-			if (player_count > 0 && old_curtime == curtime && g_pointers->m_sv->IsActive())
+			if (player_count > 0 && g_pointers->m_sv->IsActive() && tick_count > 100 && old_curtime == curtime)
 			{
 				Msg("HANG DETECTED, KILLING THE PROCESS");
 				exit(1);
